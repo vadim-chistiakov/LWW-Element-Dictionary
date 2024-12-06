f05271937f58371b910ac6dde3c92ab76d00d2b3
Vadim Chistiakov <titaniys007@rambler.ru>
2024-12-06 15:57:38 +0000

    Initial Commit

---
+++ LWW-Element-Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LWW-Element-Dictionary
+//
+//  Created by Vadim Chistiakov on 06.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
